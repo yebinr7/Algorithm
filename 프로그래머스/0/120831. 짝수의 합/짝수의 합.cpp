4e9e5a877e96f7b889d75ef08838dec139e04a2a
yebinr7 <73622439+yebinr7@users.noreply.github.com>
2024-12-26 11:10:34 +0900

    [level 0] Title: 짝수의 합, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.cpp
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.cpp
@@ -1,21 +1,30 @@
-#include <string>
+#include <string>	
 #include <vector>
-
-using namespace std;
-int sum;//전역변수라서 초기화가 필요없다. 0으로 초기화된다.
-
+#include <iostream>	
+using namespace std;	
 
 int solution(int n) {
-	for (int i = 1; i <= n; i++) {
-		if (i % 2 == 0)
-		sum += i;
+	vector<int> arr;
+	
+	int sum = 0;
+	
+	for (size_t i = 1; i <= n; i++)
+	{
+		if (i % 2 == 0) {
+			arr.push_back(i);
+		}
+
+	}
+
+	for (int num : arr) {
+		sum += num;
 	}
 	return sum;
 }
+
 int main() {
-
-	solution(10);	
-
-
+	solution(10);
 	return 0;
+
+	
 }
