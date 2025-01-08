ee6fac1d0dc68bb3446ee16c00385815aa2987db
yebinr7 <73622439+yebinr7@users.noreply.github.com>
2025-01-08 10:53:45 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.cpp
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.cpp
@@ -1,6 +1,8 @@
 #include <string>
 #include <vector>
 
+static int functionCallNum = 0;
+
 using namespace std;
 static int loopNum = 0;
 int solution(int num) 
@@ -11,21 +13,30 @@ int solution(int num)
     1-2. 입력된 수가 홀수라면 3을 곱하고 1을 더합니다.
     2. 결과로 나온 수에 같은 작업을 1이 될 때까지 반복합니다.
     */
-
-    while (num != 1) //1이면 탈출 
+    long long collatz = num;
+    while (collatz != 1) //1이면 탈출 
     {
         loopNum++; 
         if (loopNum>500)
             break;
 
-        if (num % 2 == 0)
-            num /= 2;
-        else if(num%2 ==1)//홀수 조건문 ..
-            num = num * 3 + 1;
+        if (collatz % 2 == 0)
+            collatz /= 2;
+        else//홀수 조건문 ..
+            collatz = collatz * 3 + 1; //여기서 int 넘어가는거 조심 
     }
 
-    if (num == 1)
+    if (collatz == 1)
         return loopNum;
     else
         return -1;
+}
+
+int main(void)
+{
+    //solution(16);
+    int answer = solution(17);
+	  int answer2 = solution(626331); //여기 488이나옴 500이 안찍힘
+ 
+	return 0;
 }
