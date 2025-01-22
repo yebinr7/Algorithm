78092504898bd2a5eee02e289040ae5be2d7a96b
yebinr7 <73622439+yebinr7@users.noreply.github.com>
2025-01-22 10:22:57 +0900

    [level 1] Title: 시저 암호, Time: 0.22 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.cpp
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.cpp
@@ -1,19 +1,17 @@
 #include <string>
 #include <vector>
-
+#include <sstream>
 using namespace std;
-
-string solution(string s, int n)
-{
-    string answer = "";
-    for (size_t i = 0; i < s.size(); i++)
-	{
-        if (s[i] == ' ')
-            answer += ' ';
-        else if (s[i] >= 'a' && s[i] <= 'z')
-            answer += ((s[i] + n) - 'a') % 26 + 'a';
-        else if (s[i] >= 'A' && s[i] <= 'Z')
-            answer += ((s[i] + n) - 'A') % 26 + 'A';
+string solution(string s, int n) {
+    ostringstream oss; // 문자열을 조립할 스트림 객체 생성
+    for (char c : s) {
+        if (c == ' ') {
+            oss << ' '; // 공백은 그대로 추가
+        } else if (islower(c)) {
+            oss << char((c - 'a' + n) % 26 + 'a'); // 소문자 처리
+        } else if (isupper(c)) {
+            oss << char((c - 'A' + n) % 26 + 'A'); // 대문자 처리
+        }
     }
-    return answer;
+    return oss.str(); // 최종 문자열 반환
 }
