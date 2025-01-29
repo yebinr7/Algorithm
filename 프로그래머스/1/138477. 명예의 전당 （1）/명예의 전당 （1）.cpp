394e45cb0053e525f58327f8d331fc5d0cfbf2ad
yebinr7 <73622439+yebinr7@users.noreply.github.com>
2025-01-29 11:41:48 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.70 ms, Memory: 3.73 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.cpp
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.cpp
@@ -2,24 +2,26 @@
 #include <algorithm>
 using namespace std;
 
-vector<int> solution(int k, vector<int> score) {
+vector<int> solution(int k, vector<int> score)
+{
     vector<int> answer;
-    vector<int> topK; // 명예의 전당 점수 목록
+    vector<int> topK;
+    //k번 4라면 4등 매반복마다 벡터에 넣기
 
-    for (int i = 0; i < score.size(); i++) {
-        topK.push_back(score[i]); // 새로운 점수 추가
+    for (size_t i = 0; i < score.size(); i++)
+    {
+        topK.emplace_back(score[i]);
+       //내림차순하고 [k-1]번 값 answer에추가
+       std::sort(topK.begin(), topK.end(), greater<int>());
+       // k개 이상이면 k번째 이후 점수는 제거
+       
+       if (topK.size() > k) 
+       { //k개보다 많아지면 버리기 
+           topK.pop_back();
+       }
 
-        // topK를 항상 내림차순 정렬 유지
-        sort(topK.begin(), topK.end(), greater<int>());
-
-        // k개 이상이면 k번째 이후 점수는 제거
-        if (topK.size() > k) {
-            topK.pop_back();
-        }
-
-        // 현재 명예의 전당의 최하위 점수를 answer에 추가
-        answer.push_back(topK.back());
+       //answer.emplace_back(topK[k - 1]);//최하위
+       answer.emplace_back(topK.back()); //k개의 마지막애 리턴
     }
-
     return answer;
 }
