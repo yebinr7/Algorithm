20b8136aefb2c67444473d5223a0331b30b3805a
yebinr7 <73622439+yebinr7@users.noreply.github.com>
2025-09-04 16:08:48 +0900

    [level 2] Title: 행렬의 곱셈, Time: 2.11 ms, Memory: 5.65 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.cpp
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.cpp
@@ -1,3 +1,4 @@
+// 9.4 행렬의 곱셈
 #include <string>
 #include <vector>
 
@@ -5,44 +6,44 @@ using namespace std;
 
 vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2) 
 {
-    vector<vector<int>> answer; // 행렬 C 
+    //vector<vector<int>> answer;
+    
 
-    int cRow = arr1.size();
-    int cCol = arr2[0].size();
+    int rows = arr1.size(); 
+    int cols = arr2[0].size();
+    //answer.reserve(rows * cols); // 미리 크기 확보 -> ;오류남 0으로 초기화해야함
+    
+    /*for (int i = 0; i < rows * cols; ++i)
+        answer.emplace_back(0);*/
+    // 이것때문에 ㅅㅄㅄㅄㅄㅂ
 
-    // C의 행 초기화
-    answer.resize(cRow); // resize는 0으로 초기화 됨
+    vector<vector<int>> answer(rows, vector<int>(cols,0));
 
-    // C의 열 모두 초기화 
-    for(int i=0; i<answer.size(); ++i)
-        answer[i].resize(cCol); // [0]만했었다면.. 0행만 모두 열이 초기화되어서 터짐 
 
-    int aRowsCol = 0; // A행의 열요소 담는
-    int bColsRow = 0; // B열의 행요소 담는 
-
-    for (int row = 0; row < cRow; ++row) // C의 행돌면 서
+    for (int row = 0; row < rows; ++row) // 행 
     {
-        
-        for (int col = 0; col < cCol; ++col) // C의 열 돌기 
+        for (int col = 0; col < cols; ++col) // 열 
         {
-            int sum = 0;
-            for (int i = 0; i < arr2.size(); ++i) // a의 열 과 b의 행 같은개수임!
+            for (int k = 0; k < arr1[0].size(); ++k) // A의 열 또는 B의 행이 대칭으로 
             {
-                sum += arr1[row][i] * arr2[i][col]; 
+               /* int a = arr1[row][k];
+                int b = arr2[k][col];
+                int ab = a * b;*/
+               answer[row][col] += arr1[row][k] * arr2[k][col];
             }
-            answer[row][col] = sum; // 
         }
     }
-
     return answer;
 }
+
+
 
 int main(void)
 {
     vector<vector<int>> answer1 = solution({ {1, 4 }, { 3, 2 }, { 4, 1} },
-                                            { {3, 3}, { 3, 3 } });
-    vector<vector<int>> answer2 = solution({ {2, 3, 2}, { 4, 2, 4 }, { 3, 1, 4 } }, 
-                                            { { 5, 4, 3 }, { 2, 4, 1 }, { 3, 1, 1 } });
+        { {3, 3}, { 3, 3 } });
+    vector<vector<int>> answer2 = solution({ {2, 3, 2}, { 4, 2, 4 }, { 3, 1, 4 } },
+        { { 5, 4, 3 }, { 2, 4, 1 }, { 3, 1, 1 } });
 
     return 0;
 }
